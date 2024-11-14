f55a8765babe4c668a8c84aa71cd301263c1d5b6
PabloG1998 <140307004+PabloG1998@users.noreply.github.com>
2024-11-14 13:07:03 -0300

    Arreglo de Footer

--- Ubuntu/platform/recover/recoverPassword.php
+++ Ubuntu/platform/recover/recoverPassword.php
@@ -67,21 +67,21 @@
                     <ul class="list-unstyled mb-0">
                         <li><a href="#" class="text-white">Alcances</a></li>
                         <li><a href="#" class="text-white">Precios</a></li>
-                        <li><a href="#" class="text-white">Trabaja con nosotros</a></li>
+                        <li><a href="../jobs/" class="text-white">Trabaja con nosotros</a></li>
                      </ul>
                 </div>
                 <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
                     <h5 class="text-uppercase">Contacto</h5>
                     <ul class="list-unstyled mb-0">
-                        <li><a href="#" class="text-white">Correo: info@empresa.com</a></li>
-                        <li><a href="#" class="text-white">Teléfono: +123 456 7890</a></li>
-                        <li><a href="#" class="text-white">Dirección: Calle Falsa 123</a></li>
+                        <li><a href="#" class="text-white">Correo: direccioncentro@centrodeformacionubuntu.com</a></li>
+                        <li><a href="#" class="text-white">Teléfono: 1165300745 - 45545310</a></li>
+                        <li><a href="#" class="text-white">Dirección: Santiago Parodi 4330 - Caseros, 3 de Febrero, Pcia. Buenos Aires</a></li>
                     </ul>
                 </div>
             </div>
         </div>
         <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-            &copy; 2024 Nombre de la Empresa. Todos los derechos reservados.
+            &copy; 2024 CENTRO DE CAPACITACION Y FORMACION UBUNTU . Todos los derechos reservados.
         </div>
     </footer>
 
